c91da3e7b7c0a28a46b10f4bb53fa5c718285559
rselmikaitis-dev <rselmikaitis@gmail.com>
2025-09-15 10:23:46 -0300

    Update lancamentos.py

--- pages/lancamentos.py
+++ pages/lancamentos.py
@@ -1,4 +1,5 @@
-
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode
