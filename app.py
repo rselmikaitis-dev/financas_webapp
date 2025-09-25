23069341b788b733433f818535cde5fcf42a1bb5
rselmikaitis-dev <rselmikaitis@gmail.com>
2025-09-25 18:16:13 -0300

    Update app.py
    Exclusão do Menu Comparativo

--- app.py
+++ app.py
@@ -296,9 +296,9 @@ def _apply_parcela_in_desc(desc: str, p: int, total: int) -> str:
 with st.sidebar:
     menu = option_menu(
         "Menu",
-        ["Dashboard Principal", "Lançamentos", "Importação", "Planejamento", "Comparativo", "Configurações"],
+        ["Dashboard Principal", "Lançamentos", "Importação", "Planejamento", "Configurações"],
         menu_icon=None,
-        icons=["", "", "", "", "", ""],
+        icons=["", "", "", "", ""],
         default_index=0
     )
 
